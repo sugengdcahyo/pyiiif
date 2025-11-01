71ab2be1ed82426a22d05730b1d74b811856b56a
Sugeng D. Cahyo <sugengdcahyo@gmail.com>
2025-11-01 22:02:03 +0700

    feat: setup logger

--- app/__init__.py
+++ app/__init__.py
@@ -1,12 +1,16 @@
+from app.logger import setup_logger
 from dotenv import load_dotenv
 from flask import Flask
 from flask_cors import CORS
-from .config import get_config
+from .config import get_config, Config
 from .errors import register_error_handlers
 from .routes.iiif import bp as iiif_bp
 from .routes.viewer import viewer_bp
 from .routes.metadata import bp as metadata_bp
 import os
+
+import logging
+from logging.handlers import RotatingFileHandler
 
 
 load_dotenv()
@@ -14,22 +18,12 @@ load_dotenv()
 
 def create_app() -> Flask:
     app = Flask(__name__)
-    app.config.from_object(get_config())
-    app.config["AWS_ACCESS_KEY_ID"] = os.getenv("AWS_ACCESS_KEY_ID", "")
-    app.config["AWS_SECRET_ACCESS_KEY"] = os.getenv(
-        "AWS_SECRET_ACCESS_KEY", "")
-    app.config["AWS_REGION"] = os.getenv("AWS_REGION", "")
-    app.config["IIIF_BACKEND"] = os.getenv("IIIF_BACKEND", "openslide").lower()
-    app.config["S3_BUCKET"] = os.getenv("S3_BUCKET", "")
-    app.config["S3_REGION"] = os.getenv("S3_REGION_NAME", "")
-    app.config["S3_PREFIX"] = os.getenv("S3_SLIDE_PREFIX", "")
-    app.config["VALID_EXTENSIONS"] = tuple(
-        ext.strip().lower() for ext in os.getenv(
-            "VALID_EXTENSIONS", ".svs,.tif,.tiff,.ndpi,.vms,.vmu,.scn,.mrxs,.bif,.tiff"
-        ).split(",") if ext.strip()
-    )
 
-    CORS(app, origins=app.config["CORS_ORIGINS"])
+    # config loader
+    app.config.from_object(Config)
+
+    # setup logging
+    setup_logger(app)
 
     # blueprint
     app.register_blueprint(metadata_bp, url_prefix="/metadata")
@@ -38,6 +32,8 @@ def create_app() -> Flask:
 
     # error handler JSON
     register_error_handlers(app)
+
+    app.logger.info("Flask IIIF app initialized")
 
     @app.get("/healthz")
     def healthz():
